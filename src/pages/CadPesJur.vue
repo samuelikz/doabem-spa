0b0a1faf8556c14b91f6881a0f635cd0fb4cd250
irlaandrade <paula.irla@gmail.com>
2020-11-17 18:45:47 -0300

    Adicionando página e componente de pessoa jurídica

---
+++ src/pages/CadPesJur.vue
@@ -0,0 +1,17 @@
+<template>
+  <q-page padding>
+    <div class="t-cadastro text-center text-h3">
+      <p>Cadastro Pessoa Jurídica</p>
+    </div>
+    <FormPesJur />
+  </q-page>
+</template>
+
+<script>
+import FormPesJur from 'components/FormPesJur'
+
+export default {
+  name: 'CadPesJur',
+  components: { FormPesJur }
+}
+</script>
