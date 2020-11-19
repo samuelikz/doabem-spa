4be9c879178624b4fb12dac9ef26e0071f7c06cd
jpbatista <joao.batista@a.ficr.edu.br>
2020-11-18 22:45:03 -0300

    Adição do banner ao container principal, falta ajustar a responsividade

--- src/pages/Pesquisar.vue
+++ src/pages/Pesquisar.vue
@@ -1,11 +1,35 @@
 <template>
  <q-page class="flex flex-center">
-    <!-- content -->
+<p>Pesquisar Casos</p>
   </q-page>
 </template>
 
 <script>
+
 export default {
   name: 'Pagepesquisar'
 }
 </script>
+
+<style lang="stylus" scoped>
+@import url('https://fonts.googleapis.com/css2?family=Nunito:wght@200&display=swap');
+#q-app > div > div > main > p{
+font-family: Nunito;
+font-style: normal;
+font-weight: 800;
+line-height: 25px;
+letter-spacing: 0.01em;
+color: #FFFFFF;
+}
+
+#q-app > div > div > main{
+background :url(../assets/banner.svg)
+background-position: center;
+background-repeat: no-repeat;
+background-size: cover; /*Ajuste de imagem para manter proporção*/
+position: relative;
+display: flex;
+justify-content: center;
+align-items: center;
+}
+</style>
