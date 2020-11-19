8c5fe4c44c5833c1f7296b8ff86229c91ffa58d9
jpbatista <joao.batista@a.ficr.edu.br>
2020-11-18 23:32:42 -0300

    Adição do input de pesquisa a página

--- src/pages/Pesquisar.vue
+++ src/pages/Pesquisar.vue
@@ -1,7 +1,8 @@
 <template>
  <q-page class="flex flex-center">
-<p>Pesquisar Casos</p>
-  </q-page>
+<input type="text" placeholder="pesquisar casos.....">
+
+</q-page>
 </template>
 
 <script>
@@ -13,15 +14,21 @@ export default {
 
 <style lang="stylus" scoped>
 @import url('https://fonts.googleapis.com/css2?family=Nunito:wght@200&display=swap');
-#q-app > div > div > main > p{
+//O CSS abaixo seria para adicionar como esta no FIGMA, com um texto acima da pesquisa
+/*#q-app > div > div > main > p{
 font-family: Nunito;
 font-style: normal;
 font-weight: 800;
 line-height: 25px;
 letter-spacing: 0.01em;
 color: #FFFFFF;
-}
-
+display: flex;
+justify-content: center;
+margin: 0px 160px;
+width: 60%;
+font-size: 2rem;
+}*/
+//Background da página
 #q-app > div > div > main{
 background :url(../assets/banner.svg)
 background-position: center;
@@ -31,5 +38,17 @@ position: relative;
 display: flex;
 justify-content: center;
 align-items: center;
+}
+
+#q-app > div > div > main > input{
+background: rgba(255, 255, 255, 0.86);
+border-radius: 40px;
+border: none;
+position: relative;
+display: flex;
+justify-content: center;
+align-items: center;
+text-align :center;
+width:50%;
 }
 </style>
